dbfaa5cfd367ee0d16f1b05fa89c5d2e87e72e92
Nick Lee <nicklee808@gmail.com>
2017-01-19 20:41:24 -1000

    setup

---
+++ indexing.sql
@@ -0,0 +1,24 @@
+\c postgres;
+
+DROP DATABASE indexed_cars;
+
+DROP USER indexed_cars_user;
+
+CREATE USER indexed_cars_user;
+
+CREATE DATABASE indexed_cars OWNER indexed_cars_user;
+
+\c indexed_cars;
+
+\i scripts/car_models.sql;
+
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
